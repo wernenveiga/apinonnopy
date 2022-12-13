0684a84be85c015dae8cce428536eb1cca21ba88
Wernen Veiga <wernen_veiga@outlook.com>
2022-12-12 23:55:14 -0300

    Update settings.py

--- apinonnopy/settings.py
+++ apinonnopy/settings.py
@@ -40,9 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
     'posts',
     'termos',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
